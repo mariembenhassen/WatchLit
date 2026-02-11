6c5c9a4711a4b2ba5991bdf38adbf0234b898ed7
MariemBH <mariem.benhassen@ensi-uma.tn>
2026-02-11 22:31:15 +0100

    Fashion Section , countDown

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,11 +5,11 @@ import Home from './pages/Home'
 function App() {
 
   return (
-    <>
+    <div className= " min-h-screen w-full overflow-x-hidden antialiased bg-white text-slate-900">
     <Routes>
     <Route path='/' element={<Home/>} />
     </Routes>
-    </>
+    </div>
   )
 }
 
