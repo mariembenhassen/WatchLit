59070634769b59b83f763251f751054391c1a1d5
MariemBH <mariem.benhassen@ensi-uma.tn>
2026-02-11 15:57:07 +0100

    added BG Video to Banner

--- Client/src/components/BannerHome.jsx
+++ Client/src/components/BannerHome.jsx
@@ -1,11 +1,36 @@
+import { bannerHomeStyles} from '../assets/dummyStyles'
 import NavBar from './NavBar'
+import video from '../assets/watchBanner2.mp4'
+import { useEffect, useRef } from 'react';
 
 const BannerHome = () => {
+  const videoRef  = useRef(null);
+  useEffect(()=>{
+    const reduceMotion = 
+    window.matchMedia &&
+    window.matchMedia("(prefers-reduced-motion : reduce)").matches;
+    if(reduceMotion && videoRef.current) {
+      videoRef.current.pause();
+      videoRef.current.removeAttribute("autoplay")
+    }
+  } , []);
   return (
-    <>
-    <NavBar/>
-    </>
-  )
-}
+    <div className={bannerHomeStyles.container}>
+      <div className={bannerHomeStyles.navbarWrapper}>
+         <NavBar/>
+         </div> 
+    {/* bg video */}
+
+    <div className={ bannerHomeStyles.videoContainer} >
+      <video ref={videoRef} className={bannerHomeStyles.video}
+      autoPlay muted loop playsInline  preload='metadata'
+      poster="/fallback.jpg" role='presentation'>
+        <source src={video} type='video/mp4'/>
+      </video>
+
+    </div>
+    </div>
+  );
+};
 
 export default BannerHome
